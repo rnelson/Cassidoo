a0f1e333788282c6f36ce99c7e4b0a8d876294aa
Ross Nelson <ross.nelson@gmail.com>
2024-08-05 09:52:39 -0500

    Add 20240805

--- Cassidoo/Cassidoo20240805_SumOfSquares.cs
+++ Cassidoo/Cassidoo20240805_SumOfSquares.cs
@@ -5,6 +5,7 @@ namespace Cassidoo;
 [SuppressMessage("ReSharper", "InconsistentNaming")]
 public static class Cassidoo20240805_SumOfSquares
 {
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20240805.cs
     public static int SumSquares(int n)
     {
         ArgumentOutOfRangeException.ThrowIfLessThan(n, 1);
