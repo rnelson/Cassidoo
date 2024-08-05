8b4cb7268b36d349315a12fc579307dbf024a9e0
Ross Nelson <ross.nelson@gmail.com>
2024-08-05 16:47:37 -0500

    Add test.sh output for old tests

--- Tests/Tests20210510.cs
+++ Tests/Tests20210510.cs
@@ -15,3 +15,25 @@ public class Tests20210510
         Assert.Equal(expected, actual);
     }
 }
+
+/*
+    rnelson@reject:/home/rnelson/dev/cassidoo $ ./test.sh Tests20210510
+    Test run for /usr/home/rnelson/dev/cassidoo/Tests/bin/Debug/net8.0/Tests.dll (.NETCoreApp,Version=v8.0)
+    Starting test execution, please wait...
+    A total of 1 test files matched the specified pattern.
+    [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.4.5+1caef2f33e (64-bit .NET 8.0.6)
+    [xUnit.net 00:00:00.25]   Discovering: Tests
+    [xUnit.net 00:00:00.29]   Discovered:  Tests
+    [xUnit.net 00:00:00.29]   Starting:    Tests
+    [xUnit.net 00:00:00.34]   Finished:    Tests
+      Passed Tests.Tests20210510.TestSameDigits(n: 1, expected: True) [8 ms]
+      Passed Tests.Tests20210510.TestSameDigits(n: 251895, expected: False) [< 1 ms]
+      Passed Tests.Tests20210510.TestSameDigits(n: 10, expected: True) [< 1 ms]
+      Passed Tests.Tests20210510.TestSameDigits(n: 251894, expected: True) [< 1 ms]
+
+    Test Run Successful.
+    Total tests: 4
+         Passed: 4
+     Total time: 0.8423 Seconds
+    rnelson@reject:/home/rnelson/dev/cassidoo $
+ */
--- Tests/Tests20240603.cs
+++ Tests/Tests20240603.cs
@@ -16,3 +16,24 @@ public class Tests20240603
         Assert.Equal(expected, actual);
     }
 }
+
+/*
+    rnelson@reject:/home/rnelson/dev/cassidoo $ ./test.sh Tests20240603
+    Test run for /usr/home/rnelson/dev/cassidoo/Tests/bin/Debug/net8.0/Tests.dll (.NETCoreApp,Version=v8.0)
+    Starting test execution, please wait...
+    A total of 1 test files matched the specified pattern.
+    [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.4.5+1caef2f33e (64-bit .NET 8.0.6)
+    [xUnit.net 00:00:00.28]   Discovering: Tests
+    [xUnit.net 00:00:00.33]   Discovered:  Tests
+    [xUnit.net 00:00:00.33]   Starting:    Tests
+    [xUnit.net 00:00:00.39]   Finished:    Tests
+      Passed Tests.Tests20240603.TestSquares(numbers: [11, 13, 15], expected: []) [6 ms]
+      Passed Tests.Tests20240603.TestSquares(numbers: [1, 2, 3, 4, 5, ...], expected: [2, 2, 4]) [1 ms]
+      Passed Tests.Tests20240603.TestSquares(numbers: [7, 8, 1, 0, 2, ...], expected: [0, 2, 8]) [< 1 ms]
+
+    Test Run Successful.
+    Total tests: 3
+         Passed: 3
+     Total time: 0.9217 Seconds
+    rnelson@reject:/home/rnelson/dev/cassidoo $
+ */
