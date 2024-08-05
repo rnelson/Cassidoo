f6747bacc4cfa5d5feedb6be9af832490d9c6351
Ross Nelson <ross.nelson@gmail.com>
2024-08-05 17:01:59 -0500

    Fix the tests for 20240701

--- Tests/Tests20240701.cs
+++ Tests/Tests20240701.cs
@@ -23,23 +23,20 @@ public class Tests20240701
 }
 
 /*
-    rnelson@reject:/home/rnelson/dev/cassidoo $ ./test.sh Tests20210510  
+    rnelson@reject:/home/rnelson/dev/cassidoo $ ./test.sh Tests20240701
     Test run for /usr/home/rnelson/dev/cassidoo/Tests/bin/Debug/net8.0/Tests.dll (.NETCoreApp,Version=v8.0)
     Starting test execution, please wait...
     A total of 1 test files matched the specified pattern.
     [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.4.5+1caef2f33e (64-bit .NET 8.0.6)
-    [xUnit.net 00:00:00.31]   Discovering: Tests
-    [xUnit.net 00:00:00.37]   Discovered:  Tests
-    [xUnit.net 00:00:00.38]   Starting:    Tests
-    [xUnit.net 00:00:00.43]   Finished:    Tests
-      Passed Tests.Tests20210510.TestSameDigits(n: 1, expected: True) [8 ms]
-      Passed Tests.Tests20210510.TestSameDigits(n: 251895, expected: False) [< 1 ms]
-      Passed Tests.Tests20210510.TestSameDigits(n: 10, expected: True) [< 1 ms]
-      Passed Tests.Tests20210510.TestSameDigits(n: 251894, expected: True) [< 1 ms]
+    [xUnit.net 00:00:00.24]   Discovering: Tests
+    [xUnit.net 00:00:00.28]   Discovered:  Tests
+    [xUnit.net 00:00:00.28]   Starting:    Tests
+    [xUnit.net 00:00:00.33]   Finished:    Tests
+      Passed Tests.Tests20240701.TestSample [4 ms]
 
     Test Run Successful.
-    Total tests: 4
-         Passed: 4
-     Total time: 0.9837 Seconds
+    Total tests: 1
+         Passed: 1
+     Total time: 0.8183 Seconds
     rnelson@reject:/home/rnelson/dev/cassidoo $
  */
