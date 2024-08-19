2ff151754f8d295d4ed321d8c87a021097a4f7fe
Ross Nelson <ross.nelson@gmail.com>
2024-08-19 10:15:43 -0500

    Shrink the solution a bit

--- Cassidoo/Cassidoo20240819_CalculateExecutionTimes.cs
+++ Cassidoo/Cassidoo20240819_CalculateExecutionTimes.cs
@@ -6,17 +6,12 @@ namespace Cassidoo;
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 public class Cassidoo20240819_CalculateExecutionTimes
 {
-    public static List<Tuple<string, int>> CalculateExecutionTimes(List<Cassidoo20240819_Event> events)
-    {
-        var names = events.Select(e => e.Name).Distinct();
-
-        return (
-                from n in names
+    public static List<Tuple<string, int>> CalculateExecutionTimes(List<Cassidoo20240819_Event> events) => (
+                from n in events.Select(e => e.Name).Distinct()
                 let start = events.First(e => e.Name.Equals(n) && "start".Equals(e.Event)).Time
                 let end = events.First(e => e.Name.Equals(n) && "end".Equals(e.Event)).Time
                 select new Tuple<string, int>(n, end - start)
             ).ToList();
-    }
 
     public record Cassidoo20240819_Event(string Name, int Time, string Event);
 }
