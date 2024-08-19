7d8799226748a159fcd9ef442e670146ed90960e
Ross Nelson <ross.nelson@gmail.com>
2024-08-19 10:38:36 -0500

    Remove unnecessary parens

--- Cassidoo/Cassidoo20240819_CalculateExecutionTimes.cs
+++ Cassidoo/Cassidoo20240819_CalculateExecutionTimes.cs
@@ -6,12 +6,11 @@ namespace Cassidoo;
 [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 public class Cassidoo20240819_CalculateExecutionTimes
 {
-    public static IEnumerable<Tuple<string, int>> CalculateExecutionTimes(List<Cassidoo20240819_Event> events) => (
+    public static IEnumerable<Tuple<string, int>> CalculateExecutionTimes(List<Cassidoo20240819_Event> events) =>
         from n in events.Select(e => e.Name).Distinct()
         let start = events.First(e => e.Name.Equals(n) && "start".Equals(e.Event)).Time
         let end = events.First(e => e.Name.Equals(n) && "end".Equals(e.Event)).Time
-        select new Tuple<string, int>(n, end - start)
-    );
+        select new Tuple<string, int>(n, end - start);
 }
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
