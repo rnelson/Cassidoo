032aa63bb0f3d36aae98fbfb7a209839aa81b5a6
Ross Nelson <ross.nelson@gmail.com>
2024-11-18 12:34:24 -0600

    Add 20241118 (maxTheStock)

---
+++ Cassidoo/Cassidoo20241118_MaxTheStock.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public class Cassidoo20241118_MaxTheStock
+{
+    public static int MaxTheStock(params IEnumerable<int> stock) =>
+        Math.Max(0, BuildCombinations(stock).Max(c => c.Item3));
+
+    [SuppressMessage("Performance", "CA1859:Use concrete types when possible for improved performance")]
+    private static IEnumerable<Tuple<int, int, int>> BuildCombinations(IEnumerable<int> stock)
+    {
+        var combinations = new List<Tuple<int, int, int>>();
+        var data = stock.ToArray();
+        
+        for (var i = 0; i < data.Length; i++)
+        for (var j = i + 1; j < data.Length; j++)
+            combinations.Add(new Tuple<int, int, int>(data[i], data[j], data[j] - data[i]));
+        
+        return combinations;
+    }
+}
---
+++ Tests/Tests20241118.cs
@@ -0,0 +1,34 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20241118
+{
+    [Theory]
+    [InlineData(5, new[] {7, 1, 5, 3, 6, 4})]
+    [InlineData(0, new[] {7, 6, 4, 3, 1})]
+    public void Test(int expected, IEnumerable<int> prices)
+    {
+        var actual = Cassidoo20241118_MaxTheStock.MaxTheStock(prices);
+        Assert.Equal(expected, actual);
+    }
+}
+
+/*
+   rnelson@splinter Cassidoo % ./test.sh Tests20241118
+   Test run for /Users/rnelson/dev/Cassidoo/Tests/bin/Debug/net8.0/Tests.dll (.NETCoreApp,Version=v8.0)
+   Starting test execution, please wait...
+   A total of 1 test files matched the specified pattern.
+   [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.4.5+1caef2f33e (64-bit .NET 8.0.4)
+   [xUnit.net 00:00:01.51]   Discovering: Tests
+   [xUnit.net 00:00:01.54]   Discovered:  Tests
+   [xUnit.net 00:00:01.54]   Starting:    Tests
+   [xUnit.net 00:00:01.57]   Finished:    Tests
+     Passed Tests.Tests20240819.Test [3 ms]
+
+   Test Run Successful.
+   Total tests: 1
+        Passed: 1
+    Total time: 2.1005 Seconds
+   rnelson@splinter Cassidoo %
+*/
