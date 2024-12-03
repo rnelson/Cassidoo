245b57886cebd33427f004e9b62523512c225d3a
Ross Nelson <ross.nelson@gmail.com>
2024-12-02 21:38:39 -0600

    Add 20241202 (rollCall)

---
+++ Cassidoo/Cassidoo20241202_ReindeerNames.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public class Cassidoo20241202_ReindeerNames
+{
+    public static IEnumerable<string?> RollCall(params IEnumerable<string> input) =>
+        input
+            .ToArray()
+            .Select(i => new string(i.ToCharArray().Reverse().ToArray()))
+            .Order()
+            .ToArray();
+}
---
+++ Tests/Tests20241202.cs
@@ -0,0 +1,27 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20241202
+{
+    [Theory]
+    [InlineData(
+        new[] { "Amine","Cassidy","Tenzy" },
+        new[] { "yzneT","ydissaC","enimA" })]
+    [InlineData(
+        new[] { "Blitzen","Comet","Cupid","Dancer","Dasher","Donner","Prancer","Rudolph","Vixen" },
+        new[] { "rennoD","nexiV","recnarP","temoC","neztilB","recnaD","dipuC","rehsaD","hploduR" })]
+    [InlineData(
+        new[] { "A","B","C" }, 
+        new[] { "A","B","C" })]
+    public void Test(IEnumerable<string> expected, IEnumerable<string> input)
+    {
+        var real = expected.ToArray();
+        var actual = Cassidoo20241202_ReindeerNames.RollCall(input).ToArray();
+        
+        Assert.Equal(real.Length, actual.Length);
+        
+        for (var i = 0; i < real.Length; i++)
+            Assert.Equal(real[i], actual[i]);
+    }
+}
