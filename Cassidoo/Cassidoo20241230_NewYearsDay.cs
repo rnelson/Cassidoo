1b55d41260e5d747056ff39724bb7d7a3434a974
Ross Nelson <ross.nelson@gmail.com>
2024-12-30 12:03:05 -0600

    Add 20241230 (NewYearsDay)

---
+++ Cassidoo/Cassidoo20241230_NewYearsDay.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+public class Cassidoo20241230_NewYearsDay
+{
+    public static string NewYearsDay(int year) => new DateOnly(year, 1, 1).DayOfWeek.ToString();
+}
---
+++ Tests/Tests20241230.cs
@@ -0,0 +1,16 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20241230
+{
+    [Theory]
+    [InlineData("Wednesday", 2025)]
+    [InlineData("Monday", 2024)]
+    public void Test(string expected, int year)
+    {
+        var actual = Cassidoo20241230_NewYearsDay.NewYearsDay(year);
+        
+        Assert.Equal(expected, actual);
+    }
+}
