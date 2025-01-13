6027800ef0e66ff113515ddc800d0f5ee2ddec66
Ross Nelson <ross.nelson@gmail.com>
2025-01-13 11:00:20 -0600

    Add test run

--- Tests/Tests20250113.cs
+++ Tests/Tests20250113.cs
@@ -28,4 +28,23 @@ public class Tests20250113(ITestOutputHelper output)
         
         Assert.Equal(expectedPhonetic, actual.NatoTelephony);
     }
+
+/*
+    [rnelson@roto cassidoo]$ ./test.sh Tests20250113
+    Restore complete (0.5s)
+        Determining projects to restore...
+        All projects are up-to-date for restore.
+      Cassidoo succeeded (0.2s) → Cassidoo/bin/Debug/net9.0/Cassidoo.dll
+      Tests succeeded (0.1s) → Tests/bin/Debug/net9.0/Tests.dll
+    [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.8.2+699d445a1a (64-bit .NET 9.0.0)
+    [xUnit.net 00:00:00.08]   Discovering: Tests
+    [xUnit.net 00:00:00.13]   Discovered:  Tests
+    [xUnit.net 00:00:00.14]   Starting:    Tests
+    [xUnit.net 00:00:00.25]   Finished:    Tests
+      Tests test succeeded (1.4s)
+
+    Test summary: total: 6, failed: 0, succeeded: 6, skipped: 0, duration: 1.4s
+    Build succeeded in 2.4s
+    [rnelson@roto cassidoo]$
+*/
 }
