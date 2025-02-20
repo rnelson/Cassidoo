3bc4439a3616daa1e2a1ebd0db86fb0a336ef56b
Ross Nelson <ross.nelson@gmail.com>
2025-02-20 10:31:26 -0600

    Add 20250217 (findShieldBreak)

---
+++ Cassidoo/Cassidoo20250217_ShieldDamage.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20250217_ShieldDamage
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20250217.cs
+    public static int FindShieldBreak(IEnumerable<int> hits, int shieldTotal)
+    {
+        var sum = 0;
+        
+        foreach (var (hit, index) in hits.Select((v, i) => (v, i)))
+        {
+            sum += hit;
+            
+            if (sum >= shieldTotal)
+                return index;
+        }
+
+        return -1;
+    }
+}
---
+++ Tests/Tests20250217.cs
@@ -0,0 +1,17 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20250217
+{
+    [Theory]
+    [InlineData(2, new[] {10, 20, 30, 40}, 50)]
+    [InlineData(-1, new[] {1, 2, 3, 4}, 20)]
+    [InlineData(0, new[] {50}, 30)]
+    public void Test(int expected, IEnumerable<int> hits, int shieldTotal)
+    {
+        var actual = Cassidoo20250217_ShieldDamage.FindShieldBreak(hits, shieldTotal);
+        
+        Assert.Equal(expected, actual);
+    }
+}
