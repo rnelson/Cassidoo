f71a0cfa089ccd0c99d3a89478056d1ea48e2d73
Ross Nelson <ross.nelson@gmail.com>
2025-04-05 11:17:45 -0500

    Add 20250331 (findLongestTimeGap)

---
+++ Cassidoo/Cassidoo20250331_FindLongestTimeGap.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20250331_FindLongestTimeGap
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20250331.cs
+    public static int FindLongestTimeGap(IEnumerable<string> times)
+    {
+        var converted = times.Select(TimeOnly.Parse).ToArray();
+        var max = 0;
+
+        if (converted.Length < 2) return 0;
+
+        for (var i = 0; i < converted.Length - 1; i++)
+            max = Math.Max(max, (int)new TimeSpan(converted[i+1].Ticks - converted[i].Ticks).TotalMinutes);
+
+        return max;
+    }
+}
---
+++ Tests/Tests20250331.cs
@@ -0,0 +1,18 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20250331
+{
+    [Theory]
+    [InlineData(0, new[] {"12:00"})]
+    [InlineData(120, new[] {"09:00", "11:00"})]
+    [InlineData(360, new[] {"14:00", "09:00", "15:00", "10:30"})] // 210?
+    [InlineData(240, new[] {"08:00", "10:00", "10:00", "14:00"})]
+    public void Test(int expected, IEnumerable<string> times)
+    {
+        var actual = Cassidoo20250331_FindLongestTimeGap.FindLongestTimeGap(times);
+        
+        Assert.Equal(expected, actual);
+    }
+}
