dc5d49ddf579d55f4abd918a5f8ad7a26cf8f7d2
Ross Nelson <ross.nelson@gmail.com>
2025-04-05 12:14:34 -0500

    Add an extension method to grab pairs, clean up the main func

--- Cassidoo/Cassidoo20250331_FindLongestTimeGap.cs
+++ Cassidoo/Cassidoo20250331_FindLongestTimeGap.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using Cassidoo.Extensions;
 
 namespace Cassidoo;
 
@@ -8,16 +9,19 @@ public static class Cassidoo20250331_FindLongestTimeGap
     // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20250331.cs
     public static int FindLongestTimeGap(IEnumerable<string> times)
     {
-        // See the unit tests (link above) for the reason that this doesn't get sorted
-        // in any way.
-        var converted = times.Select(TimeOnly.Parse).ToArray();
-        var max = 0;
-
-        if (converted.Length < 2) return 0;
-
-        for (var i = 0; i < converted.Length - 1; i++)
-            max = Math.Max(max, (int)new TimeSpan(converted[i+1].Ticks - converted[i].Ticks).TotalMinutes);
-
-        return max;
+        ArgumentNullException.ThrowIfNull(times);
+        
+        var data = times.ToArray();
+        if (data.Length == 1) return 0;
+        
+        // See the unit tests (link above) for the reason that
+        // this isn't getting sorted.
+        return data
+            .Select(TimeOnly.Parse)
+            .Pair()
+            .Max(p => GetMinutes(p.Item2, p.Item1));
     }
+    
+    private static int GetMinutes(TimeOnly one, TimeOnly two) =>
+        (int)new TimeSpan(two.Ticks - one.Ticks).TotalMinutes;
 }
---
+++ Cassidoo/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Cassidoo.Extensions;
+
+internal static class EnumerableExtensions
+{
+    /// <summary>
+    /// Chunks <paramref name="source"/> into pairs.
+    /// </summary>
+    /// <param name="source">The source data to enumerate.</param>
+    /// <typeparam name="T">The type of data in <paramref name="source"/>.</typeparam>
+    /// <returns><paramref name="source"/> split into pairs.</returns>
+    /// <remarks>
+    /// Chunks always
+    /// start with the sequentially next element, so the first chunk always starts with the first element,
+    /// the second chunk always starts with the second element, and so forth.
+    /// </remarks>
+    public static IEnumerable<Tuple<T, T>> Pair<T>(this IEnumerable<T> source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        var data = source.ToArray();
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(2, data.Length, nameof(data));
+        
+        for (var i = 0; i < data.Length - 1; i++)
+            yield return Tuple.Create(data[i], data[i + 1]);
+    }
+}
