cff849288ba88190f4fbc067f0edfb5b48f20571
Ross Nelson <ross.nelson@gmail.com>
2025-04-05 12:15:32 -0500

    Update test run

--- Tests/Tests20250331.cs
+++ Tests/Tests20250331.cs
@@ -34,19 +34,19 @@ public class Tests20250331
 
 /*
     rnelson@flux:~/dev/cassidoo $ ./test.sh Tests20250331
-    Restore complete (1.4s)
+    Restore complete (1.3s)
         Determining projects to restore...
         All projects are up-to-date for restore.
-      Cassidoo succeeded (0.4s) → Cassidoo/bin/Debug/net9.0/Cassidoo.dll
-      Tests succeeded (0.5s) → Tests/bin/Debug/net9.0/Tests.dll
+      Cassidoo succeeded (3.5s) → Cassidoo/bin/Debug/net9.0/Cassidoo.dll
+      Tests succeeded (1.2s) → Tests/bin/Debug/net9.0/Tests.dll
     [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.8.2+699d445a1a (64-bit .NET 9.0.3)
-    [xUnit.net 00:00:00.08]   Discovering: Tests
-    [xUnit.net 00:00:00.14]   Discovered:  Tests
-    [xUnit.net 00:00:00.15]   Starting:    Tests
-    [xUnit.net 00:00:00.21]   Finished:    Tests
-      Tests test succeeded (1.2s)
+    [xUnit.net 00:00:00.09]   Discovering: Tests
+    [xUnit.net 00:00:00.16]   Discovered:  Tests
+    [xUnit.net 00:00:00.17]   Starting:    Tests
+    [xUnit.net 00:00:00.26]   Finished:    Tests
+      Tests test succeeded (1.6s)
 
-    Test summary: total: 4, failed: 0, succeeded: 4, skipped: 0, duration: 1.2s
-    Build succeeded in 4.1s
+    Test summary: total: 5, failed: 0, succeeded: 5, skipped: 0, duration: 1.5s
+    Build succeeded in 8.2s
     rnelson@flux:~/dev/cassidoo $
 */
