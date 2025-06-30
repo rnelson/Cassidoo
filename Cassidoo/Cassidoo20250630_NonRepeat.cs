4b39cfd08085a3680bbb074a1b065f0bfc0da90f
Ross Nelson <ross.nelson@gmail.com>
2025-06-30 12:29:11 -0500

    Add 20250630

---
+++ Cassidoo/Cassidoo20250630_NonRepeat.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20250630_NonRepeat
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20250630.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static char NonRepeat(string input) =>
+        input.LastOrDefault(c => input.Count(ch => ch == c) == 1);
+}
---
+++ Tests/Tests20250630.cs
@@ -0,0 +1,17 @@
+﻿using Cassidoo;
+using Xunit.Abstractions;
+
+namespace Tests;
+
+public class Tests20250630
+{
+    [Theory]
+    [InlineData("candy canes do taste yummy", 'u')]
+    [InlineData("abcdefg", 'g')]
+    [InlineData("Cassidoo", 'd')]
+    public void Test(string input, char expected)
+    {
+        var actual = Cassidoo20250630_NonRepeat.NonRepeat(input);
+        Assert.Equal(expected, actual);
+    }
+}
