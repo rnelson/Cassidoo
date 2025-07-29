9f6d2221958e5cfa5d9cbfc13233a9c39d4bba07
Ross Nelson <ross.nelson@gmail.com>
2025-07-29 12:49:03 -0500

    Add 20250729

---
+++ Cassidoo/Cassidoo20250729_CanFormHexagon.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20250729_CanFormHexagon
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20250729.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static bool CanFormHexagon(IEnumerable<int> input)
+    {
+        var numbers = input.ToArray();
+        var used = new int[numbers.Length];
+        
+        if (numbers.Length != 6)
+            return false;
+
+        for (var i = 0; i < numbers.Length; i++)
+        {
+            for (var j = i + 1; j < numbers.Length; j++)
+            {
+                if (numbers[i] != numbers[j])
+                    continue;
+                
+                used[i] = 1;
+                used[j] = 1;
+                break;
+            }
+        }
+
+        return used.All(n => n == 1);
+    }
+}
---
+++ Tests/Tests20250729.cs
@@ -0,0 +1,18 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20250729
+{
+    [Theory]
+    [InlineData(new[] {2, 3, 8, 8, 2, 3}, true)]
+    [InlineData(new[] {1, 2, 3, 4, 5, 6}, false)]
+    [InlineData(new[] {2, 2, 2, 2, 2, 2, 2}, false)]
+    [InlineData(new[] {2, 2, 2, 2, 2, 2}, true)]
+    [InlineData(new[] {1, 2, 3, 2, 1, 3}, true)]
+    public void Test(IEnumerable<int> input, bool expected)
+    {
+        var actual = Cassidoo20250729_CanFormHexagon.CanFormHexagon(input);
+        Assert.Equal(expected, actual);
+    }
+}
