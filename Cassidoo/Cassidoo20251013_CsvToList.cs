80bafbe131371012ca6631bbc160480f5f4acc23
Ross Nelson <ross.nelson@gmail.com>
2025-10-13 22:05:30 -0500

    Add 20251013

---
+++ Cassidoo/Cassidoo20251013_CsvToList.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using JetBrains.Annotations;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20251013_CsvToList
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20251013.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static IEnumerable<string> CsvToList(string input)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            PrepareHeaderForMatch = args => args.Header.ToLower()
+        };
+
+        var reader = new StringReader(input.Replace("\\n", Environment.NewLine));
+        using var csv = new CsvReader(reader, config);
+        return csv
+            .GetRecords<Entry>()
+            .ToList()
+            .Select(e => e.ToString());
+    }
+
+    [UsedImplicitly]
+    [SuppressMessage("ReSharper", "MemberCanBePrivate.Local")]
+    [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Local")]
+    [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+    private class Entry
+    {
+        public string Name { get; init; } = string.Empty;
+        public int Age { get; init; }
+        public string City { get; init; } = string.Empty;
+
+        public override string ToString() => $"{Name}, age {Age}, from {City}";
+    }
+}
---
+++ Tests/Tests20251013.cs
@@ -0,0 +1,14 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20251013
+{
+    [Theory]
+    [InlineData("name,age,city\\n\"Ryu, Mi-yeong\",30,\"Seoul\"\\nZoey,24,\"Burbank\"", new[] {"Ryu, Mi-yeong, age 30, from Seoul", "Zoey, age 24, from Burbank"})]
+    public void Test(string input, IEnumerable<string> expected)
+    {
+        var actual = Cassidoo20251013_CsvToList.CsvToList(input);
+        Assert.Equal(expected, actual);
+    }
+}
