e09fe2ec9e0bf44763d50de0048434520c51e053
Ross Nelson <ross.nelson@gmail.com>
2025-10-23 23:45:24 -0500

    Add sample run

--- Tests/Tests20251020.cs
+++ Tests/Tests20251020.cs
@@ -13,3 +13,22 @@ public class Tests20251020
         Assert.Equal(expected, actual);
     }
 }
+
+/*
+    [rnelson@roto cassidoo]$ ./test.sh Tests20251020
+    Restore complete (0.5s)
+        Determining projects to restore...
+        All projects are up-to-date for restore.
+      Cassidoo succeeded (0.2s) → Cassidoo/bin/Debug/net9.0/Cassidoo.dll
+      Tests succeeded (0.1s) → Tests/bin/Debug/net9.0/Tests.dll
+    [xUnit.net 00:00:00.00] xUnit.net VSTest Adapter v2.8.2+699d445a1a (64-bit .NET 9.0.10)
+    [xUnit.net 00:00:00.09]   Discovering: Tests
+    [xUnit.net 00:00:00.15]   Discovered:  Tests
+    [xUnit.net 00:00:00.15]   Starting:    Tests
+    [xUnit.net 00:00:00.20]   Finished:    Tests
+      Tests test succeeded (0.9s)
+
+    Test summary: total: 2, failed: 0, succeeded: 2, skipped: 0, duration: 0.9s
+    Build succeeded in 1.9s
+    [rnelson@roto cassidoo]$
+*/
