da078e39f52928ab228882d58b64414d790e11b2
Ross Nelson <ross.nelson@gmail.com>
2025-10-23 23:43:27 -0500

    Add 20251020

---
+++ Cassidoo/Cassidoo20251020_SplitByWidths.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20251020_SplitByWidths
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20251020.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static IEnumerable<string> SplitByWidths(string input, IEnumerable<int> widths)
+    {
+        if (!widths.Any())
+            throw new ArgumentOutOfRangeException(nameof(widths), widths, "Must have at least one width");
+        
+        if (string.IsNullOrWhiteSpace(input))
+            return [];
+
+        var result = new List<string>();
+        
+        var w = new Stack<int>(widths.Reverse());
+        var remaining = input;
+        var currentWidth = w.Pop();
+
+        while (!string.IsNullOrEmpty(remaining))
+        {
+            if (remaining.Length <= currentWidth)
+            {
+                result.Add(remaining);
+                break;
+            }
+            
+            result.Add(remaining.Substring(0, currentWidth));
+            remaining = remaining.Substring(currentWidth);
+
+            // We don't care about the success here; either it's updated or it stays the same.
+            if (w.TryPop(out var newWidth))
+                currentWidth = newWidth;
+        }
+
+        return result;
+    }
+}
---
+++ Tests/Tests20251020.cs
@@ -0,0 +1,15 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20251020
+{
+    [Theory]
+    [InlineData("Supercalifragilisticexpialidocious", new[] { 5, 9, 4 }, new[] { "Super", "califragi", "list", "icex", "pial", "idoc", "ious" })]
+    [InlineData("I'm just an adult and life is a nightmare", new[] { 4, 5, 3, 6, 4 }, new[] { "I'm ", "just ", "an ", "adult ", "and ", "life", " is ", "a ni", "ghtm", "are" })]
+    public void Test(string input, IEnumerable<int> widths, IEnumerable<string> expected)
+    {
+        var actual = Cassidoo20251020_SplitByWidths.SplitByWidths(input, widths);
+        Assert.Equal(expected, actual);
+    }
+}
