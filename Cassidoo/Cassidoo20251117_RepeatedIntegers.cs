d2649bc85cd41e423b66b7474003ea5251719751
Ross Nelson <ross.nelson@gmail.com>
2025-11-17 11:57:43 -0600

    Create files for 20251117

---
+++ Cassidoo/Cassidoo20251117_RepeatedIntegers.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20251117_RepeatedIntegers
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20251117.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static IEnumerable<int> RepeatedIntegers(int n)
+    {
+        return [];
+    }
+}
---
+++ Tests/Tests20251117.cs
@@ -0,0 +1,14 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20251117
+{
+    [Theory]
+    [InlineData(4, new[]{1, 2, 2, 3, 3, 3, 4, 4, 4, 4})]
+    public void Test(int n, IEnumerable<int> expected)
+    {
+        var actual = Cassidoo20251117_RepeatedIntegers.RepeatedIntegers(n);
+        Assert.Equal(expected, actual);
+    }
+}
