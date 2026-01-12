6fe4d66c828acb7d3422041e436d0d2f94947f6e
Ross Nelson <ross.nelson@gmail.com>
2026-01-12 17:57:58 -0600

    Add 20260112 HungryBears

---
+++ Cassidoo/Cassidoo20260112_HungryBears.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+public static class Cassidoo20260112_HungryBears
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260112.cs
+    [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+    public static IEnumerable<string> HungryBears(IEnumerable<Bear> bears)
+    {
+        var average = (uint)(bears.Sum(b => b.Hunger) / bears.Count());
+        return bears
+            .Where(b => b.Hunger > average)
+            .OrderBy(b => b.Name)
+            .Select(b => b.Name);
+    }
+
+    [SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
+    public record Bear(string Name, uint Hunger);
+    
+    
+    
+    public static Bear[] TestBears => new List<Bear>
+    {
+        new("Baloo", 6),
+        new("Yogi", 9),
+        new("Paddington", 4),
+        new("Winnie", 10),
+        new("Chicago", 20),
+    }.ToArray();
+}
---
+++ Tests/Tests20260112.cs
@@ -0,0 +1,14 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20260112
+{
+    [Fact]
+    public void Test()
+    {
+        var expected = new[] { "Chicago", "Winnie" };
+        var actual = Cassidoo20260112_HungryBears.HungryBears(Cassidoo20260112_HungryBears.TestBears).ToArray();
+        Assert.Equal(expected, actual);
+    }
+}
