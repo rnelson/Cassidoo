332bf05633d1661e1f9066b06301d5296e19043c
Ross Nelson <ross.nelson@gmail.com>
2026-06-29 22:49:47 -0500

    LINQify

--- Cassidoo/Cassidoo20260629_MeanBits.cs
+++ Cassidoo/Cassidoo20260629_MeanBits.cs
@@ -8,16 +8,7 @@ public static class Cassidoo20260629_MeanBits
 {
     // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260629.cs
     public static double NiceBits(int n) => MeanBits(n);
-    
-    public static double MeanBits(int n)
-    {
-        var sum = 1;
-        
-        for (var i = 1; i < n; i++)
-        {
-            sum += Convert.ToString(i, 2).Length;
-        }
 
-        return (double)sum / n;
-    }
+    public static double MeanBits(int n) =>
+        Enumerable.Range(1, n - 1).Aggregate(1d, (sum, i) => sum + Convert.ToString(i, 2).Length) / n;
 }
