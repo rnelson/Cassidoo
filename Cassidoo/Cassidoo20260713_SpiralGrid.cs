fac972fea9df193acba39fd5b104e699d8dae6f9
Ross Nelson <ross.nelson@gmail.com>
2026-07-13 13:40:38 -0500

    Add the shell for 20260713 SpiralGrid

---
+++ Cassidoo/Cassidoo20260713_SpiralGrid.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Cassidoo;
+
+[SuppressMessage("ReSharper", "InconsistentNaming")]
+[SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+public static class Cassidoo20260713_SpiralGrid
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260713.cs
+    public static string SpiralGrid(int n)
+    {
+        return string.Empty;
+    }
+}
---
+++ Tests/Tests20260713.cs
@@ -0,0 +1,14 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20260713
+{
+    [Theory]
+    [InlineData(0, "")]
+    public void Test(int n, string expected)
+    {
+        var actual = Cassidoo20260713_SpiralGrid.SpiralGrid(n);
+        Assert.Equal(expected, actual);
+    }
+}
