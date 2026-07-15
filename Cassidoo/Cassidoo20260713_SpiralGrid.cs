d2edc508a6b2681298a4a0fb50f5e2988fafd15b
Ross Nelson <ross.nelson@gmail.com>
2026-07-15 10:41:25 -0500

    WIP commit

--- Cassidoo/Cassidoo20260713_SpiralGrid.cs
+++ Cassidoo/Cassidoo20260713_SpiralGrid.cs
@@ -11,6 +11,11 @@ public static class Cassidoo20260713_SpiralGrid
     // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260713.cs
     public static string SpiralGrid(int n)
     {
+        Direction.Right.CollisionChange = Direction.Down;
+        Direction.Down.CollisionChange = Direction.Left;
+        Direction.Left.CollisionChange = Direction.Up;
+        Direction.Up.CollisionChange = Direction.Right;
+        
         var dim = (uint)Math.Ceiling(Math.Sqrt(n + 1));
         var grid = new SquareStringGrid(dim);
 
@@ -19,14 +24,23 @@ public static class Cassidoo20260713_SpiralGrid
 
         for (var i = 0; i <= n; i++)
         {
-            if (location.Item1 > dim || location.Item2 > dim || grid[location.Item1, location.Item2] is not null)
+            // Add the number to the grid
+            grid[location.Item1, location.Item2] = i.ToString();
+            
+            // Continue along the path
+            location = (location.Item1 + direction!.XInc, location.Item2 + direction.YInc);
+            
+            // If we're out of bounds or the new location is already in use, undo that last move then change direction
+            if (location.Item1 >= dim ||
+                location.Item2 >= dim ||
+                !grid.ContainsPoint(location.Item1, location.Item2) ||
+                grid[location.Item1, location.Item2] is not null)
             {
+                location = (location.Item1 - direction!.XInc, location.Item2 - direction.YInc);
+                
                 direction = direction.CollisionChange;
                 location = (location.Item1 + direction!.XInc, location.Item2 + direction.YInc);
             }
-            
-            grid[location.Item1, location.Item2] = i.ToString();
-            location = (location.Item1 + direction!.XInc, location.Item2 + direction.YInc);
         }
 
         return grid.ToString();
@@ -45,13 +59,5 @@ public static class Cassidoo20260713_SpiralGrid
         public static readonly DirectionalDetail Down = new(0, 1);
         public static readonly DirectionalDetail Left = new(-1, 0);
         public static readonly DirectionalDetail Up = new(0, -1);
-
-        public Direction()
-        {
-            Right.CollisionChange = Down;
-            Down.CollisionChange = Left;
-            Left.CollisionChange = Up;
-            Up.CollisionChange = Right;
-        }
     }
 }
--- Cassidoo/Collections/StringGrid.cs
+++ Cassidoo/Collections/StringGrid.cs
@@ -12,43 +12,47 @@ public class StringGrid
     {
         _rows = rows;
         _columns = columns;
-        _grid = new string[_rows, _columns];
+        _grid = new string[_columns, _rows];
         
         for (var row = 0; row < _rows; row++)
         for (var col = 0; col < _columns; col++)
-            _grid[row, col] = null;
+            _grid[col, row] = null;
     }
 
     public string? this[int x, int y]
     {
-        get => _grid[x, y];
-        set => _grid[x, y] = value;
+        get => _grid[y, x];
+        set => _grid[y, x] = value;
     }
+    
+    public bool ContainsPoint(int x, int y) => y < _rows && x < _columns && _grid[y, x] != null;
 
     public override string ToString()
     {
         var largest = 0;
         for (var row = 0; row < _rows; row++)
         for (var col = 0; col < _columns; col++)
-            if (_grid[row, col] is not null && _grid[row, col]?.Length > largest)
-                largest = _grid[row, col]!.Length;
+            if (_grid[col, row] is not null && _grid[col, row]?.Length > largest)
+                largest = _grid[col, row]!.Length;
 
         var size = largest > 0 ? largest + 1 : 0;
 
         var sb = new StringBuilder();
-        
+
         for (var row = 0; row < _rows; row++)
-        for (var col = 0; col < _columns; col++)
-            if (_grid[row, col] is null)
-            {
-                for (var i = 0; i < size; i++) sb.Append(' ');
-            }
-            else
-            {
-                sb.Append(' ');
-                sb.Append(_grid[row, col]);
-            }
-        
+        {
+            for (var col = 0; col < _columns; col++)
+                if (_grid[row, col] is not null)
+                {
+                    sb.Append(_grid[row, col]!.PadLeft(size));
+                    sb.Append(' ');
+                }
+            
+            var temp = sb.ToString().TrimEnd() + "\n";
+            sb.Clear();
+            sb.Append(temp);
+        }
+
         return sb.ToString().TrimEnd();
     }
 }
