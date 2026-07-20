4ede40db9051b9220b8c7c9f7bcd1acfa981576b
Ross Nelson <ross.nelson@gmail.com>
2026-07-20 15:56:59 -0500

    Add 20260720_CountBlinks

---
+++ Cassidoo/Cassidoo20260720_CountBlinks.cs
@@ -0,0 +1,36 @@
+﻿namespace Cassidoo;
+
+public static class Cassidoo20260720_CountBlinks
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260720.cs
+    public static int CountBlinks(string input) => input
+        .Substrings('_')
+        .Where(s => !string.IsNullOrEmpty(s))
+        .Select(s => s.Substring(1, s.Length - 2))
+        .OrderByDescending(s => s.Length)
+        .FirstOrDefault()?
+        .Length ?? 0;
+}
+
+public static class Cassidoo20260720_StringExtensions
+{
+    public static IEnumerable<string> Substrings(this string input, char marker)
+    {
+        var first = input.IndexOf(marker);
+        var last = input.LastIndexOf(marker);
+
+        if (first < 0 || last < 0 || first == last)
+        {
+            yield return string.Empty;
+            yield break;
+        }
+
+        for (var i = first; i < last;)
+        {
+            var next = input.IndexOf(marker, i + 1);
+            yield return input.Substring(i, next - i + 1);
+
+            i = next;
+        }
+    }
+}
---
+++ Tests/Tests20260720.cs
@@ -0,0 +1,15 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20260720
+{
+    [Theory]
+    [InlineData("_..__...._.", 4)]
+    [InlineData("...._", 0)]
+    public void Test(string input, int expected)
+    {
+        var actual = Cassidoo20260720_CountBlinks.CountBlinks(input);
+        Assert.Equal(expected, actual);
+    }
+}
