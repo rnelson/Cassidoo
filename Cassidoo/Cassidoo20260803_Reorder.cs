eacbf9a332e2f689e2a36ba9db78999652d0728c
Ross Nelson <ross.nelson@gmail.com>
2026-08-03 15:45:47 -0500

    Add 20260803_Reorder

---
+++ Cassidoo/Cassidoo20260803_Reorder.cs
@@ -0,0 +1,13 @@
+﻿namespace Cassidoo;
+
+public static class Cassidoo20260803_Reorder
+{
+    // Tests: https://github.com/rnelson/Cassidoo/blob/main/Tests/Tests20260803.cs
+    public static IEnumerable<string> Reorder(IEnumerable<string> strings, IEnumerable<uint> indexes)
+    {
+        return indexes
+            .Zip(strings, (idx, str) => new { Index = idx, Value = str })
+            .OrderBy(o => o.Index)
+            .Select(o => o.Value);
+    }
+}
---
+++ Tests/Tests20260803.cs
@@ -0,0 +1,16 @@
+﻿using Cassidoo;
+
+namespace Tests;
+
+public class Tests20260803
+{
+    [Theory]
+    [InlineData(new[]{"C", "D", "E", "F", "G", "H"},
+        new uint[]{3, 0, 4, 1, 2, 5},
+        new[]{"D", "F", "G", "C", "E", "H"})]
+    public void Test(string[] values, uint[] indexes, string[] expected)
+    {
+        var actual = Cassidoo20260803_Reorder.Reorder(values, indexes).ToArray();
+        Assert.Equal(expected, actual);
+    }
+}
